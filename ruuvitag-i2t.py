fa888d0c490507aba2f3d11525052185d02b0c87
Gustavo Belbruno <gustavobelbruno@gmail.com>
2021-03-20 18:48:27 +0000

    added Bundle creation and update

--- ruuvitag-i2t.py
+++ ruuvitag-i2t.py
@@ -1,18 +1,24 @@
+import sys, threading, time, json
 from datetime import datetime
+sys.path.insert(1, './i2t-lib')
+from iot2tangle import Bundle, Sender
 from bluepy.btle import Scanner, DefaultDelegate
-import threading, time
 from decoderf5 import Df5Decoder
-import json
+
 
 ### LOAD 'config.json' FILE ###
 config_json = json.load(open("config.json"))
-device_name = config_json['MAC-Ruuvitag-devices']
+device_names = config_json['MAC-Ruuvitag-devices']
 http_url = config_json['HTTP-address']
 interval = config_json['interval']
 ###############################
 
+bundle = Bundle(device_names)
+
+dec = Df5Decoder()	# Decoder Init
+
 def foo():
-    print("Interrupcion")
+    print(bundle.get_json())
     time.sleep(1)
 
 def timestamp():
@@ -26,11 +32,10 @@ class ScanDelegate(DefaultDelegate):
 		DefaultDelegate.__init__(self)
 
 	def handleDiscovery(self, dev, isNewDev, isNewData):
-		if (any(dev.addr == i for i in device_name)):
-			dec = Df5Decoder()
+		if (any(dev.addr == i for i in device_names)):
 			data = dec.json_i2t(dev.rawData)
+			bundle.update(dev.addr, data)	# Update device data in the Bundle Class
 
-			print(datetime.now().time(), data, dev.addr)
 			scanner.clear()
 			scanner.start()
 
